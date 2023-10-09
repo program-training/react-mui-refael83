6f22e2d5dc3a23a5849a6071c69548b9e44b733f
רפאל אדרי <you@example.com>
2023-10-09 16:38:27 +0300

    step 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Example } from "./components/Example/Example";
 import Bag from "./components/Bag/Bag";
+import ModalB from "./components/Modal/ModalB";
 
 function App() {
   return (
     <>
       <Bag/>
+      <ModalB/>
       <Example />
     </>
   );
